3fe13e9743a3ebd7bb6ae79513465a2baa23b0d1
navaneethginson00-stack <navaneethginson00@gmail.com>
2026-02-26 01:53:38 +0530

    Connected contact form to backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,25 +1,49 @@
 function toggleMode() {
     document.body.classList.toggle("dark");
 }
-document.addEventListener("DOMContentLoaded", function() {
 
-    document.getElementById("contactForm").addEventListener("submit", function(e) {
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("contactForm").addEventListener("submit", async function (e) {
         e.preventDefault(); // Stop page refresh
 
         let name = document.getElementById("name").value;
         let email = document.getElementById("email").value;
         let message = document.getElementById("message").value;
 
-        if(name === "" || email === "" || message === "") {
+        if (name === "" || email === "" || message === "") {
             alert("Please fill all fields!");
             return;
         }
 
-        document.getElementById("successMsg").innerHTML = "✅ Message Sent Successfully!";
-        document.getElementById("successMsg").style.color = "#00ffff";
+        try {
+            const response = await fetch("/contact", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    message: message
+                })
+            });
 
-        document.getElementById("contactForm").reset();
+            const data = await response.json();
+
+            if (data.success) {
+                document.getElementById("successMsg").innerHTML = "✅ Message Sent Successfully!";
+                document.getElementById("successMsg").style.color = "#00ffff";
+                document.getElementById("contactForm").reset();
+            } else {
+                alert("Error: " + data.message);
+            }
+
+        } catch (error) {
+            console.error("Error:", error);
+            alert("Server error. Try again.");
+        }
+
     });
-    
 
 });
